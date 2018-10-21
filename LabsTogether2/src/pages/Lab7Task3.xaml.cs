7caf5b3b20937fe8cb0fb89f5201e36f17372b2e
gameloo <gameloo@mail.ru>
2018-10-21 17:00:21 +0300

    Fix Lab7Task3

--- LabsTogether2/src/pages/Lab7Task3.xaml.cs
+++ LabsTogether2/src/pages/Lab7Task3.xaml.cs
@@ -25,33 +25,41 @@ namespace LabsTogether2.src.pages
     /// </summary>
     public sealed partial class Lab7Task3 : Page
     {
-        private ObservableCollection<Student> students;
-        private List<LevelOfEducation> levelOfEducations;
+        private ObservableCollection<Student> studentsResponse;
+        private Hashtable students;
 
         public Lab7Task3()
         {
             this.InitializeComponent();
-            students = new ObservableCollection<Student>();
-            levelOfEducations = Enum.GetValues(typeof(LevelOfEducation)).Cast<LevelOfEducation>().ToList();
-            
+            students = new Hashtable
+            {
+                { 123456, new Student("Иван", "Иванов", "Иванович", 12345, LevelOfEducation.Bachelor) },
+                { 123457, new Student("Артем", "Иванов", "Иванович", 12345, LevelOfEducation.Bachelor) },
+                { 123458, new Student("Лидия", "Иванова", "Иванович", 12345, LevelOfEducation.Bachelor) },
+                { 123459, new Student("Артем", "Кузнецов", "Иванович", 12345, LevelOfEducation.Bachelor) },
+                { 123450, new Student("Степан", "Студентов", "Иванович", 12345, LevelOfEducation.Bachelor) },
+                { 123451, new Student("Степан", "Иванов", "Иванович", 12345, LevelOfEducation.Bachelor) },
+                { 123452, new Student("Артем", "Логвинов", "Иванович", 12345, LevelOfEducation.Bachelor) }
+            };
+            studentsResponse = new ObservableCollection<Student>();
+
         }
 
-        private async void AddNewStudent(object sender, RoutedEventArgs e)
+        private async void FindStudent(object sender, RoutedEventArgs e)
         {
             try
             {
-                students.Add(new Student(tbFN.Text, tbLN.Text, tbP.Text, Int32.Parse(tbPID.Text), (LevelOfEducation)cbLOE.SelectedItem));
+                var selectedStudents = from t in students.Values.OfType<Student>().ToList()
+                                       where t.LastName == tbLN.Text
+                                       select t;
+                studentsResponse = new ObservableCollection<Student>(selectedStudents);
+                lvStudents.ItemsSource = studentsResponse;
                 lvStudents.UpdateLayout();
             }
             catch (Exception ex)
             {
                 await other.Window.ShowErrorWindowAsync(ex);
             }
-        }
-
-        private void RemoveStudent(object sender, RoutedEventArgs e)
-        {
-            students.Remove((Student)lvStudents.SelectedItem);
         }
     }
 }
